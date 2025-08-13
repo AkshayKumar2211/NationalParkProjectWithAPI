8014f7c586f691c0bb184375b514e79c3e998659
AkshayKumar2211 <taara0361@gmail.com>
2025-08-13 21:44:41 +0530

    Add project files.

---
+++ NationalParkApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NationalParkApi.Models;
+
+namespace NationalParkApi.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
+        }
+
+        public DbSet<NationalPark>  NationalParks { get; set; }
+        public DbSet<Trail> Trailers { get; set; }
+    }
+}
