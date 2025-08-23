188db88836edadc09cbf884457b29f883d96bfdd
AkshayKumar2211 <taara0361@gmail.com>
2025-08-23 18:02:48 +0530

    "Consume API in /mvc"

---
+++ NationalParkWebAPPForConsume/Models/Trail.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NationalParkWebAPPForConsume.Models
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Distance { get; set; }
+        [Required]
+        public string Elevation { get; set; }
+        public DateTime DateCreated { get; set; }
+        public enum DifficultyType { Easy, Moderate, Difficult };
+
+        public DifficultyType Difficulty { get; set; }
+        [Display(Name ="National Park")]
+        public int NationalParkId { get; set; }
+        [ForeignKey("NationalParkId")]
+        public NationalPark NationalPark { get; set; }
+    }
+}
---
+++ NationalParkWebAPPForConsume/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace NationalParkWebAPPForConsume
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "https://localhost:7286/";
+        public static string NationalParkAPIPath=APIBaseUrl + "api/nationalPark";
+        public static string TrailAPIPath = APIBaseUrl+"api/trail";
+    }
+}
